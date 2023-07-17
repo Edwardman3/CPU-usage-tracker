b7776bff8a62a3ad92b4f1e9a85636ef00e36d39
Edwardman3 <g.bytnar98@gmail.com>
2023-07-17 15:02:46 +0200

    Added 3 thread and mutex implementation

--- main.c
+++ main.c
@@ -8,11 +8,8 @@
 #include <signal.h>
 #include <errno.h>
 
-#define MAX_SIZE_OF_QUEUE 200
+#define MAX_SIZE_OF_QUEUE 20100
 
-volatile sig_atomic_t done = 0;
-int nCore = 4;
-int nMember = 10;
 
 
 typedef struct ProcStat 
@@ -32,23 +29,18 @@ struct queue {
 
 CPUstat** ArrayOfData;
 struct queue* dataReader;
-
-void initQueue(){
-	dataReader=(struct queue*)malloc(sizeof(struct queue));
-	dataReader->prevData = NULL;
-	dataReader->nextData = NULL;
-	dataReader->nIter = 1;
-	dataReader->actualData.nMember = nMember;
-}
-
+pthread_mutex_t lock;
+volatile sig_atomic_t done = 0;
+int nCore;
+int nMember;
 
 void destroyQueue(struct queue *fromData){
 	struct queue* tempData;
 	struct queue* tempPrevData;
 	assert(fromData != NULL);
+	
 	if(fromData != NULL){
 		tempData=fromData->nextData;
-		
 		while(fromData != NULL){
 			if(fromData->actualData.member != NULL) {
 				free(fromData->actualData.member);
@@ -67,7 +59,23 @@ void destroyQueue(struct queue *fromData){
 				tempData = tempData->nextData;
 			}
 		}
-	}	
+		else{
+			dataReader = tempData;
+		}
+	}
+}
+		
+void stopProgram(int sig){
+	done = 0;
+	puts("CTR + C detected");
+}
+
+void initQueue(){
+	dataReader=(struct queue*)malloc(sizeof(struct queue));
+	dataReader->prevData = NULL;
+	dataReader->nextData = NULL;
+	dataReader->nIter = 1;
+	dataReader->actualData.nMember = nMember;
 }
 
 void rewindData(int nIter){ // 0 set to the top, 1,2,3.. iter to down, -1, -2,-3,-4... iter to top
@@ -104,7 +112,6 @@ void rewindData(int nIter){ // 0 set to the top, 1,2,3.. iter to down, -1, -2,-3
 	}
 }
 
-
 int addDataQueue(){
 	FILE* dataFile;
 	char line[200];
@@ -119,10 +126,7 @@ int addDataQueue(){
 	}
 	
 	fgets(line, 200, dataFile);
-	
-	//Cleaning old data of Queue
-	
-	puts("START");
+	//puts("START");
 	while(counter_1 < nCore){
 		
 		if(dataReader == NULL){
@@ -135,11 +139,11 @@ int addDataQueue(){
 		}
 		else{
 			if (dataReader->nIter == MAX_SIZE_OF_QUEUE){
-				rewindData(MAX_SIZE_OF_QUEUE - 4);
-				printf("%d \n", dataReader->nIter);
+				rewindData(MAX_SIZE_OF_QUEUE - (nCore));
+				//printf("%d \n", dataReader->nIter);
 				destroyQueue(dataReader);
 				rewindData(0);
-				printf("%d \n", dataReader->nIter);
+			//	printf("%d \n", dataReader->nIter);
 			}
 			dataReader->nextData = malloc(sizeof(struct queue));
 			assert(dataReader->nextData != NULL);
@@ -183,8 +187,7 @@ void printStat(){
 	}
 }
 
-void initialArray()
-{
+void initialArray(){
 	assert(nCore > 0);
 	int counter_1 = 0;
 	int counter_2 = 0;
@@ -225,9 +228,7 @@ void initialArray()
 	}
 }
 
-
-void freeArray()
-{
+void freeArray(){
 	assert(nCore > 0);
 	int counter_1 = 0;
 	int counter_2 = 0;
@@ -245,22 +246,22 @@ void freeArray()
 	free(ArrayOfData);
 }
 
-void term(){
-	puts("zakonczono");
-	freeArray(4);
-	done = 1;
-}
 
-void checkData() {
+int checkData() {
 	FILE* data = NULL;
 	char line[200];
 	int counter = 0;
 	
+	errno = 0;
+	data = fopen("/proc/stat", "r");
+	if (errno != 0){
+		perror("Error occured. File not open");
+		return 1;
+	}
 	
 	assert(nMember == 0);
 	assert(nCore == 0);
 	
-	data = fopen("/proc/stat", "r");
 	assert(data!=NULL);
 	
 	fgets(line, 200, data);
@@ -280,6 +281,7 @@ void checkData() {
 	}while((strtok(line, " "))[0] == 'c'); // c is the first char of CPU
 	(nCore)--;
 	fclose(data);
+	return 0;
 }
 
 void checkDataTest() {
@@ -291,8 +293,7 @@ void checkDataTest() {
 	assert(b == 10); 		//spec value for my system
 }
 
-void makeDataOld()
-{
+void makeDataOld(){
 	CPUstat* temp = ArrayOfData[0];
 	ArrayOfData[0] = ArrayOfData[1];
 	ArrayOfData[1] = temp;
@@ -386,7 +387,60 @@ void printCPUusage(){
 	1 - old data
 */
 
-
+void getArrayFromQueue(){
+	int temp_rewind_iter = 0;
+	int counter_1 = 0;
+	int	counter_2 = 0;
+	//puts("check");
+	rewindData(4);
+	//printf("Liczba danych: %d , nastepne dane: %p \n ",dataReader->nIter, dataReader->nextData);
+	if(dataReader == NULL){	
+		rewindData(1);
+		//puts("rewind");
+	}
+	//printf("Liczba danych: %d , nastepne dane: %p \n ",dataReader->nIter, dataReader->nextData);
+	while((dataReader->nIter % 4 != 0) && (dataReader->actualData.member!=NULL)){
+		rewindData(1);
+		//puts("rewind");
+	}
+	//printf("Liczba danych: %d , nastepne dane: %p \n ",dataReader->nIter, dataReader->nextData);
+	while(counter_1 < nCore) {
+		//printf("Liczba danych: %d , nastepne dane: %p \n",dataReader->nIter, dataReader->nextData);
+		strcpy(ArrayOfData[0][counter_1].name, dataReader->actualData.name);
+		while(counter_2 < ArrayOfData[0][counter_1].nMember) {
+			ArrayOfData[0][counter_1].member[counter_2] = dataReader->actualData.member[counter_2];
+			counter_2++;
+		}
+		rewindData(1);
+		counter_1++;
+		counter_2 = 0;
+	}
+	
+	counter_1 = 0;
+	counter_2 = 0;
+	temp_rewind_iter = dataReader->nIter - nCore;
+	rewindData(temp_rewind_iter);
+	
+	while(counter_1 < nCore) {
+		//printf("Liczba danych: %d , nastepne dane: %p \n",dataReader->nIter, dataReader->nextData);
+		strcpy(ArrayOfData[1][counter_1].name, dataReader->actualData.name);
+		while(counter_2 < ArrayOfData[1][counter_1].nMember) {
+			ArrayOfData[1][counter_1].member[counter_2] = dataReader->actualData.member[counter_2];
+			counter_2++;
+		}
+		rewindData(1);
+		counter_1++;
+		counter_2 = 0;
+	}
+	rewindData((-1)*(temp_rewind_iter+(2*nCore)-1));
+	//printf("Liczba danych: %d , nastepne dane: %p \n",dataReader->nIter, dataReader->nextData);
+	//puts("Przed zniszczeniem");
+	destroyQueue(dataReader);
+	//printf("Liczba danych: %d , nastepne dane: %p \n",dataReader->nIter, dataReader->nextData);
+	rewindData(0);
+	//printf("Liczba danych: %d , nastepne dane: %p \n",dataReader->nIter, dataReader->nextData);
+	//puts("KONIEC");
+}
 
 void calcCPUusage(){
 	unsigned long long int PrevIdle 	= 0;
@@ -417,98 +471,117 @@ void calcCPUusage(){
 		
 		
 		assert(totald > 0);
-		
+		pthread_mutex_lock(&lock);
 		ArrayOfData[0][i].usage = 100*(totald - idled)/(double)totald;
+		pthread_mutex_unlock(&lock);
 	}
 }
 
 void *threadReader(){
-	getData();
-	calcCPUusage();
+	puts("T1");
+	addDataQueue();
 	return NULL;
 }
 
 void *threadAnalyzer(){
-	calcCPUusage();
+	puts("T2");
+	if (dataReader->nIter >= 10000){
+		assert(dataReader->nIter >= 10000);
+		puts("T2.2");
+		pthread_mutex_lock(&lock);
+		getArrayFromQueue();
+		assert(dataReader->nIter < 20000);
+		pthread_mutex_unlock(&lock);
+		calcCPUusage();
+	}
 	return NULL;
 }
 
 void *threadPrinter(){
+	puts("T3");
+	pthread_mutex_lock(&lock);
 	printCPUusage();
+	pthread_mutex_unlock(&lock);
 	return NULL;
 }
 
+void testAnalyzer(){
+	
+	int count = 40000;
+	checkData();
+	initialArray();
+	while(count>0){
+		addDataQueue();
+		count--;
+	}
+	printf("Liczba danych: %d , nastepne dane: %p \n ",dataReader->nIter, dataReader->nextData);
+	getArrayFromQueue();
+	printf("Liczba danych: %d , nastepne dane: %p \n ",dataReader->nIter, dataReader->nextData);
+	calcCPUusage();
+    printf("Liczba danych: %d , nastepne dane: %p , poprzednie dane: %p \n ",dataReader->nIter, dataReader->nextData, dataReader->prevData);
+	printCPUusage();
+	puts("test END");
+	addDataQueue();
+}
 
 int main(int argc, char* argv[]) {
 	
+	//testAnalyzer();
 	
-	int counter_main = 0;
+	done = 1;
+	int counter_task = 0;
+	int ended_task [5] ={0,0,0,0,0};
+	pthread_t th[5];
+	clock_t time_of_work[5];
 	
-	while(counter_main < 200){
-		addDataQueue();
-		printf("Iter: %d ILOSC DANYCH: %d \n", counter_main,dataReader->nIter);
-		counter_main++;
-	}	
-	destroyQueue(dataReader);
-	/*
-	
-	addDataQueue();
-	printStat();
-	sleep(1);
-	addDataQueue();
-	printStat();
-	sleep(1);
-	addDataQueue();
-	printStat();
-	
-	rewindData(3);
-	printf("ILOSC DANYCH po przewinieciu: %d \n", dataReader->nIter);
-	printStat();
-	rewindData(0);
-	printf("ILOSC DANYCH: %d \n", dataReader->nIter);
-	printStat();
-	printf("ILOSC DANYCH: %d \n", dataReader->nIter);
-	printf("poprzedni: %p | nastepnt: %p | iter: %d \n", dataReader->prevData->prevData, dataReader->prevData->nextData, dataReader->prevData->nIter);
-	assert(dataReader != NULL);
-	destroyQueue(dataReader->prevData->prevData->prevData->prevData);
-	printf("poprzedni: %p | nastepnt: %p | iter: %d \n", dataReader->prevData, dataReader->nextData, dataReader->nIter);
-	puts("Po usunieciu");
-	printStat();
-	//printf("poprzedni: %p | nastepnt: %p | iter: %d", dataReader->prevData, dataReader->nextData, dataReader->nIter);
-	printf(" %p \n", dataReader);
-	destroyQueue(dataReader);
-	printf(" %p \n", dataReader);
-	//destroyQueue(dataReader);
-	//printf(" %p \n", dataReader);
-	//int testIteration = 0;
-	*/
-	/*
-	 * struct sigaction action;
-	memset(&action, 0, sizeof(struct sigaction));
-	action.sa_handler = term;
-	sigaction(SIGTERM, &action, NULL);
+	signal(SIGINT, stopProgram);
 	checkData();
 	initialArray();
-	getData();
-	pthread_t thread[5];
-	pthread_create(&thread[2], NULL, &threadPrinter, NULL);
-	while(!done){
-		pthread_create(&thread[0], NULL, &threadReader, NULL);
-		//pthread_create(&thread[1], NULL, &threadAnalyzer, NULL);
-		if (pthread_join(thread[2], NULL) == 0) {
-			pthread_create(&thread[2], NULL, &threadPrinter, NULL);
-		}
-		sleep(1)
-		getData(nCore);
-		calcCPUusage(nCore);ny
-		system("clear");
-		printCPUusage(nCore);
-		//printCPUstat(nCore,1);
-		//printCPUstat(nCore,0);
-		//checkDataTest();
-		testIteration++;
+	
+	if (pthread_mutex_init(&lock, NULL) != 0)
+	{
+		printf("MUTES INIT FAILED!!!");
+		return 1;
 	}
-	* 
-	*/
+	
+	time_of_work[2] = clock();
+
+	while(done)
+	{
+		if (pthread_join(th[0], NULL) != 0 ){
+			pthread_create(&(th[0]), NULL, &threadReader, NULL);
+			time_of_work[0] = clock();
+		}
+
+		if (pthread_join(th[1], NULL) != 0 ){
+			pthread_create(&(th[1]), NULL, &threadAnalyzer, NULL);
+			time_of_work[1] = clock();
+		}
+		
+		if ((pthread_join(th[2], NULL) != 0 ) && (clock()-time_of_work[2] > 2000)){
+			pthread_create(&(th[2]), NULL, &threadPrinter, NULL);
+			time_of_work[2] = clock();
+		}
+		
+	}
+	
+
+	while(counter_task < 5){
+		while(ended_task[counter_task] == 0){
+			ended_task[counter_task] = pthread_join(th[counter_task], NULL);
+			if (ended_task[counter_task]){
+				printf("End task %d \n",counter_task + 1);
+			}
+		}
+		counter_task++;
+	}
+	
+	
+	pthread_mutex_destroy(&lock);
+	//printf("Liczba danych: %d , nastepne dane: %p \n ",dataReader->nIter, dataReader->nextData);
+	destroyQueue(dataReader);
+	freeArray();
+	puts("im OUT");
+	sleep(100);
 	return 0;
 }
